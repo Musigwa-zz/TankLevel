fb6fa4b83838fec49c3183db3cdf48cc9eb6ce29
Musigwa <pacifique.musigwa@gmail.com>
2018-07-17 23:57:03 +0200

    Added some auth

--- CoinTest.ino
+++ CoinTest.ino
@@ -10,35 +10,34 @@
 // volatile int CulseCount;
 // unsigned long oldTime;
 // float userEntered = 0;
+// bool coinInserted = false;
 
 // /********************************************************************************/
 
 // void setup()
 // {
-//  Serial.begin(115200);
-//  pinMode(COIN_PIN, INPUT), digitalWrite(COIN_PIN, HIGH);
-//  CulseCount, oldTime = 0;
-//  attachInterrupt(COIN_INTERRUPT, CulseCounter, FALLING);
+//   Serial.begin(115200);
+//   CulseCount, oldTime = 0, attachInterrupt(COIN_INTERRUPT, CulseCounter, RISING);
 // };
 
 // /********************************************************************************/
 
 // void loop()
 // {
-//  if ((millis() - oldTime) > 1000)
-//  {
-//    detachInterrupt(COIN_INTERRUPT), userEntered += CulseCount * COIN_AMOUNT;
-//    Serial.print("AMOUNT: "), Serial.println(String(userEntered) + "RWF");
-//    CulseCount = 0, attachInterrupt(COIN_INTERRUPT, CulseCounter, FALLING);
-//    oldTime = millis();
-//  }
+//   if ((millis() - oldTime) > 1000 && coinInserted)
+//   {
+//     detachInterrupt(COIN_INTERRUPT), userEntered += CulseCount * COIN_AMOUNT;
+//     Serial.print("AMOUNT: "), Serial.println(String(userEntered) + "RWF");
+//     CulseCount = 0, attachInterrupt(COIN_INTERRUPT, CulseCounter, RISING);
+//     oldTime = millis(), coinInserted = false;
+//   }
 // };
 
 // /********************************************************************************/
 
 // void CulseCounter()
 // {
-//  CulseCount++;
+//   coinInserted = true, CulseCount++;
 // };
 
 // /****************************** END *********************************************/
